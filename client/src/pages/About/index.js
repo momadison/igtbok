59151b72e8184c97083a3918076be4a3a0a0a602
javierordz <javier.ordz@gmail.com>
2019-07-03 20:41:41 -0500

    fonts imported, PVSA added

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -7,7 +7,7 @@ function About() {
 
             <section className="container">
                 <h1>History</h1>
-                <p>It’s Going To Be Ok Inc. (IGTBOK) is a 170(b)(1)(A)(vi) non-profit charity started in 2014 by Tonya Stafford, a survivor herself of human trafficking, in Dallas, Texas. 
+                <p><b>It’s Going To Be Ok Inc. (IGTBOK)</b> is a 170(b)(1)(A)(vi) non-profit charity started in 2014 by Tonya Stafford, a survivor herself of human trafficking, in Dallas, Texas.
                     The IGTBOK mission is to create a just world where women, children, and all vulnerable adults are safe, respected, and valued. The vision is to eradicate human trafficking, one survivor at a time, regardless of ethnicity or background.
                     With these principles, IGTBOK has grown in recognition throughout the state of Texas and continues reaching lives across the entire United States.</p>
             </section>
@@ -15,30 +15,32 @@ function About() {
             <section className="container">
                 <h1>Our Founder</h1>
                 <div className="founder-pic">
-                    <img className="board-img" src=".\assets\img\Tonya-Stafford-BW.jpg" alt="Tonya Stafford" srcset=".\assets\img\Tonya-Stafford-BW-200x200.jpg 200w, .\assets\img\Tonya-Stafford-BW-400x400.jpg 400w, .\assets\img\Tonya-Stafford-BW.jpg 500w" sizes="(max-width: 800px) 100vw, 500px" style={{width: "100%"}}/>
+                    <img className="board-img" src=".\assets\img\Tonya-Stafford-BW.jpg" alt="Tonya Stafford" srcset=".\assets\img\Tonya-Stafford-BW-200x200.jpg 200w, .\assets\img\Tonya-Stafford-BW-400x400.jpg 400w, .\assets\img\Tonya-Stafford-BW.jpg 500w" sizes="(max-width: 800px) 100vw, 500px" style={{ width: "100%" }} />
                 </div>
                 <div className="founder-vid">
-                    <iframe src="https://www.youtube.com/embed/4PqqKMivKEQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" alt="Tonya Stafford interview" allowfullscreen style={{width: "100%", height:"100%"}}/>
+                    <iframe src="https://www.youtube.com/embed/4PqqKMivKEQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" alt="Tonya Stafford interview" allowfullscreen style={{ width: "100%", height: "100%" }} />
                 </div>
 
-                <p>Tonya Stafford’s passion for helping others comes as an active response to a traumatic life — a childhood history of physical abuse, sexual molestation, rape, and ultimately, slavery. 
-                    After receiving a scholarship from Dr. Frowsa Booker-Drew with World Vision, Tonya found Greater Dallas Movement Day, a human trafficking awareness program sponsored by the City of Dallas, where victims, supporters, and advocates shared their stories. 
-                    There, she first shared the story of how her life had been changed forever and how she made it out. That moment sparked the dream of starting a non-profit to help others become free in the same way, a dream became a reality with It’s Going To Be OK, Inc.</p>
+                <p>Tonya Stafford’s passion for helping others comes as an active response to a traumatic life — a childhood history of physical abuse, sexual molestation, rape, and ultimately, slavery.
+                    After receiving a scholarship from Dr. Frowsa Booker-Drew with World Vision, Tonya found Greater Dallas Movement Day, a human trafficking awareness program sponsored by the City of Dallas,
+                    where victims, supporters, and advocates shared their stories.
+                    There, she first shared the story of how her life had been changed forever and how she made it out.
+                    That moment sparked the dream of starting a non-profit to help others become free in the same way, a dream became a reality with IGTBOK.</p>
                 <p>Tonya is a native of Dallas, Texas with a degree in Early Childhood Education and over 10 years of experience working with women, young girls, and children. She is:</p>
                 <ul>
                     <li>a Certified Mediator,</li>
                     <li>a Registered Level Trainer with The Texas Early Childhood Professional Development System (TECPDS),</li>
-                    <li>a two-term Board Member for Head Start of Greater Dallas (HSGD), Texas Head Start Association (THSA), and HSGD Policy Council, and </li>
-                    <li>a National Community Liaison for Dallas Men Winning Against Domestic Violence (DMWDV)</li>
-                    <li>a volunteer with the Prostitution Diversion Initiative (For The Sake Of One) of the Dallas Police Department</li>
+                    <li>a two-term Board Member for Head Start of Greater Dallas (HSGD), Texas Head Start Association (THSA), and HSGD Policy Council, </li>
+                    <li>a National Community Liaison for Dallas Men Winning Against Domestic Violence (DMWDV),</li>
+                    <li>a volunteer with the Prostitution Diversion Initiative (For The Sake Of One) of the Dallas Police Department, and</li>
                     <li>certified by Texas Against Sexual Assault (TAASA).</li>
                 </ul>
-                <p>Her commitment is to the education of best safety and humanitarian practices and to speaking advocacy for women and children silenced by death and tragedy. 
+                <p>Her commitment is to the education of best safety and humanitarian practices and to speaking advocacy for women and children silenced by death and tragedy.
                     Tonya has testified to the Texas State Senate Hearing Committee for Human Trafficking, contributing to the State of Texas recognizing January as Human Trafficking Month by passing HB 2290 on September 1, 2015.</p>
 
-                <p>Tonya Stafford is a consummate grassroots professional and expert in the domestic violence and human trafficking epidemics that plagues our community. 
-                    Her experience brings awareness and education to the public by identifying current and potential victims, while creating solutions to combat the roots on local and worldwide levels. 
-                    As a result, Dallas County has chosen to stand with her and It’s Going To Be Ok, Inc. by presented a proclamation on January 5, 2016 in honor of their dedication to eradicating human trafficking. 
+                <p>Tonya Stafford is a consummate grassroots professional and expert in the domestic violence and human trafficking epidemics that plagues our community.
+                    Her experience brings awareness and education to the public by identifying current and potential victims, while creating solutions to combat the roots on local and worldwide levels.
+                    As a result, Dallas County has chosen to stand with her and It’s Going To Be Ok, Inc. by presented a proclamation on January 5, 2016 in honor of their dedication to eradicating human trafficking.
                     Tonya shares her story to help others to see there is hope after trafficking, abuse and neglect.</p>
             </section>
 
@@ -229,7 +231,7 @@ function About() {
             </section>
 
         </main>
-            )
-        }
-        
+    )
+}
+
 export default About;
--- client/src/pages/Impact/index.js
+++ client/src/pages/Impact/index.js
@@ -6,11 +6,17 @@ function About() {
         <main>
 
             <section className="container">
-                <h1>Impact</h1>
-                <p>It’s Going To Be Ok Inc. (IGTBOK) is a 170(b)(1)(A)(vi) non-profit charity started in 2014 by Tonya Stafford, a survivor herself of human trafficking, in Dallas, Texas. 
-                    The IGTBOK mission is to create a just world where women, children, and all vulnerable adults are safe, respected, and valued. The vision is to eradicate human trafficking, one survivor at a time, regardless of ethnicity or background.
-                    With these principles, IGTBOK has grown in recognition throughout the state of Texas and continues reaching lives across the entire United States.</p>
+                <h1>President's Volunteer Service Award</h1>
+                <p>The President’s Volunteer Service Award (PVSA) is the premier federal volunteer program, in it's own words, "recognizing, celebrating, and holding up as role models Americans making a positive impact as engaged and deeply committed volunteers." 
+                    <b>It’s Going To Be Ok Inc. (IGTBOK)</b> is proud to be both officially PVSA-recognized and a certifying organization for individuals seeking to join. 
+                    We encourage all volunteers interested in assisting IGTBOK and other advocacy groups to register, document their hours, and inspire others to a similar life of service!
+                </p>
+
+                <button>About PVSA</button>
+                <button>Registration</button>
+
             </section>
+
 
         </main>
             )
