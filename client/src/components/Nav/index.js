e017a3b272c8ed9974bc483e958995b7d9ea0762
javierordz <javier.ordz@gmail.com>
2019-07-03 23:10:27 -0500

    nav link effect

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Nav.css"
 
 function Nav() {
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg bg-dark">
             <a className="navbar-brand" href="/">
                 <img src="assets/img/IGTBOK-Brand-Logo.png" height="70" alt="It's Going To Be OK" />
             </a>
@@ -13,19 +14,19 @@ function Nav() {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-md-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/donate">Donate</a>
+                        <a className="nav-link" href="/donate" data-hover="Donate">Donate</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/resources">Get Help</a>
+                        <a className="nav-link" href="/resources" data-hover="Get Help">Get Help</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/about">About</a>
+                        <a className="nav-link" href="/about" data-hover="About">About</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/impact">Impact</a>
+                        <a className="nav-link" href="/impact" data-hover="Impact">Impact</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/contact">Contact</a>
+                        <a className="nav-link" href="/contact" data-hover="Contact">Contact</a>
                     </li>
                 </ul>
             </div>
