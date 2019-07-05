d2989fa4b7b71bdb4313ecbce5b0b3466b138371
javierordz <javier.ordz@gmail.com>
2019-07-05 14:38:45 -0500

    PVSA added

--- client/src/pages/Impact/index.js
+++ client/src/pages/Impact/index.js
@@ -4,24 +4,20 @@ import "./style.css";
 function About() {
     return (
         <main>
-
             <section className="container">
                 <h1>President's Volunteer Service Award</h1>
-                <img className="pres-img" src="./assets/img/PVSA-banner.jpg" />
+                <img className="pres-img" src="./assets/img/PVSA-banner_comp.png" />
                 <div className="btn-group">
-                    <button className="btn btn-1 btn-1a">About PVSA</button>
-                    <button className="btn btn-1 btn-1a">Registration</button>
+                    <button className="btn-1 btn-1b">About PVSA</button>
+                    <button className="btn-1 btn-1b">Registration</button>
                 </div>
                 <p>The President’s Volunteer Service Award (PVSA) is the premier federal volunteer program, in it's own words, "recognizing, celebrating, and holding up as role models Americans making a positive impact as engaged and deeply committed volunteers." 
                     <b>It’s Going To Be Ok Inc. (IGTBOK)</b> is proud to be both officially PVSA-recognized and a certifying organization for individuals seeking to join. 
                     We encourage all volunteers interested in assisting IGTBOK and other advocacy groups to register, document their hours, and inspire others to a similar life of service!
                 </p>
-
             </section>
-
-
         </main>
-            )
-        }
+    )
+}
         
 export default About;
