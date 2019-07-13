7a45525075889bbc56a8de7678d76b1eb5d85595
javierordz <javier.ordz@gmail.com>
2019-07-13 15:09:54 -0500

    merge setup for Resources

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,11 @@ import Donate from "./pages/Donate";
 import About from "./pages/About/";
 import Impact from "./pages/Impact/";
 import Blog from "./pages/Blog/";
-import AllBlogs from "./pages/AllBlogs";
-import FullBlogPost from "./pages/FullBlogPost";
-// import HopePearls from "./pages/HopePearls";
 import Gallery from "./pages/Gallery";
 import Programs from "./pages/Programs";
+import Resources from "./pages/Resources";
 import Admin from "./pages/Admin"
-// import Contact from "./components/Contact/"
+import Contact from "./pages/Contact/"
 import PrivateRoute from './components/Auth/PrivateRoute'
 
 // components
@@ -30,12 +28,11 @@ function App() {
           <Route exact path="/Donate/" component={Donate} />
           <Route exact path="/About" component={About} />
           <Route exact path="/Impact" component={Impact} />
+          <Route exact path="/About" component={About} />
           <Route exact path="/gallery" component={Gallery} />
           <Route exact path="/Blog/" component={Blog} />
-          <Route exact path="/AllBlogs/" component={AllBlogs} />
-          <Route exact path="/FullBlogPost/:id" component={FullBlogPost} />
+          <Route exact path="/Resources" component={Resources} />
           <Route exact path="/programs" component={Programs} />
-          {/* <Route exact path="/blog" component={Wizard} /> */}
           <Route exact path="/Contact" component={Contact} />
           <Route exact path="/loggedin" component={() => <h1 style={{textAlign: 'center'}}>Logged In Successfully</h1>} />
           <Route exact path="/loggedout" component={() => <h1 style={{textAlign: 'center'}}>Logged Out Successfully</h1>} />
