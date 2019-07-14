0452e1ce9c204c91f55555b095c927fff3a3767c
Matthew Cole <colematthewg@gmail.com>
2019-07-14 16:33:08 -0500

    blog pulling images & styled-sort of

--- client/src/components/BlogAll/index.js
+++ client/src/components/BlogAll/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+import "./style.css";
 
 class BlogAll extends Component {
     state = {
         blogs: []
     }
-
     componentDidMount() {
         axios.get("api/blog").then(
             blogs => {this.setState({blogs:
@@ -14,20 +13,18 @@ class BlogAll extends Component {
             console.log(this.state.blogs)}
         )
     }
-
     render() {
         return (
             <div>
-
             {this.state.blogs.map(blog => (
                 <div className="card flex-row flex-wrap">
                 <div className="card-header border-0">
-                    <img src="//placehold.it/150" alt="" />
+                    <img className="allBlogIMG" src={blog.blogImageURL} alt={blog.blogTitle} />
                 </div>
                 <div className="card-block px-2">
                     <h4 className="card-title">{blog.blogTitle}</h4>
                     <p className="card-text">{blog.blogBody.substring(0, 125)}</p>
-                    <a href={"/FullBlogPost/" + blog._id} className="btn btn-primary">View Full Article</a>
+                    <a href={"/FullBlogPost/" + blog._id} className="btn btn-danger">View Full Article</a>
                 </div>
                 <div className="w-100"></div>
                 {/* <div className="card-footer w-100 text-muted">
